97ba01a9603ab54acf41a2048ba6b9cb5a6e52c6
Matt Ruttan <matthew.ruttan5@gmail.com>
2017-10-12 14:24:17 -0400

    Updated directory and file sructure

--- client/main.js
+++ client/main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './components/app';
+import App from '../imports/ui/app';
 
 Meteor.startup(() => {
   ReactDOM.render(<App />, document.querySelector('.render-target'));
