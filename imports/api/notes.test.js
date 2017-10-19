d46973196966f0726caf6e0280283c8cb3dc466f
Matt Ruttan <matthew.ruttan5@gmail.com>
2017-10-19 11:09:22 -0400

    Testing Meteor Publications

--- imports/api/notes.test.js
+++ imports/api/notes.test.js
@@ -12,10 +12,17 @@ if (Meteor.isServer) {
       updatedAt: 0,
       userId: 'testUserId1'
     };
-
+    const noteTwo = {
+      _id: 'testNoteId2',
+      title: 'Things to Buy',
+      body: 'Couch',
+      updatedAt: 0,
+      userId: 'testUserId2'
+    };
     beforeEach(function () {
       Notes.remove({});
       Notes.insert(noteOne);
+      Notes.insert(noteTwo);
     });
 
     it('should insert new note', function () {
@@ -104,6 +111,21 @@ if (Meteor.isServer) {
       expect(() => {
         Meteor.server.method_handlers['notes.update'].apply({ userId: noteOne.userId });
       }).toThrow();
+    });
+
+    it('should return a users notes', function () {
+      const res = Meteor.server.publish_handlers.notes.apply({ userId: noteOne.userId });
+      const notes = res.fetch();
+
+      expect(notes.length).toBe(1);
+      expect(notes[0]).toEqual(noteOne);
+    });
+
+    it('should return no notes for user that has none', function () {
+      const res = Meteor.server.publish_handlers.notes.apply({ userId: 'testid' });
+      const notes = res.fetch();
+
+      expect(notes.length).toBe(0);
     });
   });
 }
