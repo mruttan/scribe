12c06c911ba85c132d6008ba3c8900abae7495a4
Matt Ruttan <matthew.ruttan5@gmail.com>
2017-10-20 14:56:34 -0400

    Fixed bug where cursor jumps to end on update by switching from props to state in editor & Added in Delete Note functionality

--- imports/routes/routes.js
+++ imports/routes/routes.js
@@ -3,48 +3,49 @@ import React from 'react';
 import { Router, Route, browserHistory } from 'react-router';
 import { Session } from 'meteor/session';
 
-import Dashboard from '../ui/Dashboard';
-import Login from '../ui/Login';
 import Signup from '../ui/Signup';
+import Dashboard from '../ui/Dashboard';
 import NotFound from '../ui/NotFound';
+import Login from '../ui/Login';
 
-const unauthenticatedPages = ['/', '/signup'];
-const authenticatedPages = ['/dashboard'];
-const onEnterPublicPage = () => {
-	if (Meteor.userId()) {
-		browserHistory.replace('/dashboard');
-	}
-};
-const onEnterPrivatePage = () => {
-	if (!Meteor.userId()) {
-		browserHistory.replace('/');
-	}
-};
 const onEnterNotePage = (nextState) => {
-	if (!Meteor.userId()) {
-		browserHistory.replace('/');
-	} else {
-		// Uses nextState object to assign selectedNoteId to Id in url,
-		// allowing note to be already selected on page enter/refresh
-		Session.set('selectedNoteId', nextState.params.id);
-	}
+	// Uses nextState object to assign selectedNoteId to Id in url, allowing note to be already selected on page enter/refresh
+	Session.set('selectedNoteId', nextState.params.id);
 };
-export const onAuthChange = (isAuthenticated) => {
-	const pathname = browserHistory.getCurrentLocation().pathname;
-	const isUnauthenticatedPage = unauthenticatedPages.includes(pathname);
-	const isAuthenticatedPage = authenticatedPages.includes(pathname);
+const onLeaveNotePage = () => {
+	// Clear session variable so when logging back in, no auto selected note
+	Session.set('selectedNoteId', undefined);
+};
+export const onAuthChange = (isAuthenticated, currentPagePrivacy) => {
+	const isUnauthenticatedPage = currentPagePrivacy === 'unauth';
+	const isAuthenticatedPage = currentPagePrivacy === 'auth';;
+	// console.log('isAuthenticated', isAuthenticated);
+	// logs true to browser if currently logged in, false otherwise, great for dev
+
 	if (isUnauthenticatedPage && isAuthenticated) {
 		browserHistory.replace('/dashboard');
 	} else if (isAuthenticatedPage && !isAuthenticated) {
 		browserHistory.replace('/');
 	}
 };
+//use nextState object routes array to set privacy value of last page visited (lastRoute)
+export const globalOnChange = (prevState, nextState) => {
+	globalOnEnter(nextState);
+};
+export const globalOnEnter = (nextState) => {
+	const lastRoute = nextState.routes[nextState.routes.length - 1];
+	Session.set('currentPagePrivacy', lastRoute.privacy);
+	//currentPagePrivacy is set to "privacy" prop of whatever Route it is on
+}
+//adding a custom "privacy" prop - we are making this up to set up authorization
 export const routes = (
-  <Router history={browserHistory}>
-    <Route path="/" component={Login} onEnter={onEnterPublicPage}/>
-    <Route path="/signup" component={Signup} onEnter={onEnterPublicPage}/>
-    <Route path="/dashboard" component={Dashboard} onEnter={onEnterPrivatePage}/>
-		<Route path="/dashboard/:id" component={Dashboard} onEnter={onEnterNotePage}/>
-    <Route path="*" component={NotFound}/>
-  </Router>
+	<Router history={browserHistory}>
+		<Route onEnter={globalOnEnter} onChange={globalOnChange}>
+			<Route path="/" component={Login} privacy="unauth" />
+			<Route path="/signup" component={Signup} privacy="unauth" />
+			<Route path="/dashboard" component={Dashboard} privacy="auth" />
+			<Route path="/dashboard/:id" component={Dashboard} privacy="auth" onEnter={onEnterNotePage} onLeave={onLeaveNotePage}/>
+			<Route path="*" component={NotFound}/>
+		</Route>
+	</Router>
 );
--- imports/ui/Editor.js
+++ imports/ui/Editor.js
@@ -3,29 +3,54 @@ import { createContainer } from 'meteor/react-meteor-data';
 import { Session } from 'meteor/session';
 import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
+import { browserHistory } from 'react-router';
 
 import { Notes } from '../api/notes';
 
 
 export class Editor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      body: ''
+    };
+  }
   handleBodyChange(e) {
-    this.props.call('notes.update', this.props.note._id, {
-      body: e.target.value
-    });
+    const body = e.target.value;
+    this.setState({ body })
+    this.props.call('notes.update', this.props.note._id, { body });
   }
   handleTitleChange(e) {
-    this.props.call('notes.update', this.props.note._id, {
-      title: e.target.value
-    });
+    const title = e.target.value;
+    this.setState({ title });
+    this.props.call('notes.update', this.props.note._id, { title });
+  }
+  handleRemoval() {
+    this.props.call('notes.remove', this.props.note._id);
+    this.props.browserHistory.push('/dashboard');
+  }
+  componentDidUpdate(prevProps, prevState) {
+    const currentNoteId = this.props.note ? this.props.note._id : undefined;
+    const prevNoteId = prevProps.note ? prevProps.note._id : undefined;
+
+    if (currentNoteId && currentNoteId !== prevNoteId) {
+      this.setState({
+        title: this.props.note.title,
+        body: this.props.note.body
+      });
+    }
   }
 	render() {
 		if(this.props.note) {
+     // switching from props to state to fix a cursor bug where
+     // react is able to preserve cursor position on update
 			return (
         <div>
           <div className="row wrapper border-bottom white-bg page-heading">
             <div className="col-lg-12">
                 <h2>
-                  <input value={this.props.note.title} placeholder="Your title here" onChange={this.handleTitleChange.bind(this)}/>
+                  <input value={this.state.title} placeholder="Your title here" onChange={this.handleTitleChange.bind(this)}/>
                 </h2>
             </div>
           </div>
@@ -34,9 +59,9 @@ export class Editor extends React.Component {
                 <div className="col-lg-12">
                     <div className="text-center m-t-lg">
                       <p>
-                        <textarea value={this.props.note.body} placeholder="Your note here" onChange={this.handleBodyChange.bind(this)}></textarea>
+                        <textarea value={this.state.body} placeholder="Your note here" onChange={this.handleBodyChange.bind(this)}></textarea>
                       </p>
-                      <button>Delete Note</button>
+                      <button onClick={this.handleRemoval.bind(this)}>Delete Note</button>
                     </div>
                 </div>
             </div>
@@ -58,7 +83,7 @@ export class Editor extends React.Component {
                 <div className="col-lg-12">
                     <div className="text-center m-t-lg">
                         <small>
-                          {this.props.selectedNoteId ? 'The note you are looking for doesn\'t exist' : 'Please click Create Note to get started.'}
+                          {this.props.selectedNoteId ? 'The note you are looking for doesn\'t exist' : 'Select or Create a Note to get started.'}
                         </small>
                     </div>
                 </div>
@@ -72,7 +97,9 @@ export class Editor extends React.Component {
 
 Editor.propTypes = {
 	note: PropTypes.object,
-	selectedNoteId: PropTypes.string
+	selectedNoteId: PropTypes.string,
+  call: PropTypes.func.isRequired,
+  browserHistory: PropTypes.object.isRequired
 }
 
 export default createContainer(() => {
@@ -81,6 +108,7 @@ export default createContainer(() => {
 	return {
 		selectedNoteId,
 		note: Notes.findOne(selectedNoteId),
-    call: Meteor.call
+    call: Meteor.call,
+    browserHistory
 	};
 }, Editor);
