115ef0fce98c84e79c46cfc56608b29f0527c1fd
Matt Ruttan <matthew.ruttan5@gmail.com>
2017-10-20 09:42:10 -0400

    Implementing Seed Project Sidebar HTML

--- imports/ui/NoteList.js
+++ imports/ui/NoteList.js
@@ -6,12 +6,37 @@ import { createContainer } from 'meteor/react-meteor-data';
 import { Notes } from '../api/notes';
 import NoteListHeader from './NoteListHeader';
 
+// Need to update button css, but everything works!
 export const NoteList = (props) => {
   return (
-    <div>
-      <NoteListHeader/>
-      NoteList { props.notes.length }
-    </div>
+    <nav className="navbar-default navbar-static-side" role="navigation">
+        <div className="sidebar-collapse">
+
+            <a className="close-canvas-menu"><i className="fa fa-times"></i></a>
+
+            <ul className="nav" id="side-menu">
+                <li className="nav-header">
+                    <div className="profile-element">
+                        <a>
+                            <span className="clear block m-t-xs"> <strong className="font-bold">
+                              <NoteListHeader/>
+                            </strong></span>
+                        </a>
+                    </div>
+                    <div className="logo-element">
+                        IN+
+                    </div>
+                </li>
+                <li className="">
+                    <a href=""><i className="fa fa-dashboard"></i>
+                    <span className="nav-label">
+                      NoteList { props.notes.length }
+                    </span> </a>
+                </li>
+            </ul>
+
+        </div>
+    </nav>
   );
 };
 
